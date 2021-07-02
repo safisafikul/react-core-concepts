0481f2ba9e746989ae85bde27a48ac8d1c4aee5b
safi <safisafikuls@gmail.com>
2021-07-02 23:20:40 +0600

    practice

--- src/App.js
+++ src/App.js
@@ -6,12 +6,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>I am a React Learner</p>
-        
-        
+        <p>I am a React Learner!!!</p>
+        <Person name="Munna" job="Actor"></Person>
+        <Person name="Faruk" job="Footballer"></Person>
+        <Person name="Saddam" job="Driver"></Person>
       </header>
     </div>
   );
+}
+
+function Person(props) {
+  return (
+    <div style={{border: '5px solid gold', width: '400px', margin:'10px'}}>
+      <h3 style={{color: 'cyan', fontSize: '50px'}}>My name: {props.name}</h3>
+      <h3 style={{color: 'magenta', fontSize: '20px'}}>My profession: {props.job}</h3>
+    </div>
+  )
 }
 
 export default App;
