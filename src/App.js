1456336c20bea797e788e90bfbb5d07ec6780464
safi <safisafikuls@gmail.com>
2021-07-08 02:48:40 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
-// import logo from './logo.svg';
+// next 8
+
 import './App.css';
 
 function App() {
-  const nayoks = ['']
+  const nayoks = ['Anwar', 'Samman', 'Hugosab', 'Jackobs', 'Landmand', 'Hervey']
   const products = [
     { name: 'Photoshop', price: '$90.99' },
     { name: 'Illustrator', price: '$60.99' },
-    { name: 'PDF Reader', price: '$6.99' }
+    { name: 'PDF Reader', price: '$6.99' },
+    { name: 'Premiere El', price: '$249.99' }
   ]
 
   return (
     <div className="App">
       <header className="App-header">
         <p>I am a React Learner!!!</p>
-        <Product product={products[0]}></Product>
-        <Product product={products[1]}></Product>
-        <Product product={products[2]}></Product>
-        <Person name="Safi" job="Actor"></Person>
-        <Person name="Faruk" job="Footballer"></Person>
-        <Person name="Saddam" job="Driver"></Person>
+        <ul>
+          {
+            nayoks.map(nayok => <li>{nayok}</li>)
+          }
+          {
+            products.map(product => <li>{product.name}</li>)
+          }
+        </ul>
+          {
+            products.map(pd => <Product product={pd}></Product>)
+        }
       </header>
     </div>
   );
 }
 
+
 function Product(props) {
   const productStyle = {
-    border: '1px solid gray',
+    border: '5px solid purple',
     borderRadius: '5px',
     backgroundColor: 'red',
     height: '200px',
@@ -38,15 +46,6 @@ function Product(props) {
       <h3>{props.product.name}</h3>
       <h5>{props.product.price}</h5>
       <button>Buy Now</button>
-    </div>
-  )
-}
-
-function Person(props) {
-  return (
-    <div style={{border: '2px solid gold', width: '400px', margin:'10px'}}>
-      <h3>My name: {props.name}</h3>
-      <h3>My profession: {props.job}</h3>
     </div>
   )
 }
