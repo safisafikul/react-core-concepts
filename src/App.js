1f654a0a73fbc8c94eeeb74a455b333fe568145b
safi <safisafikuls@gmail.com>
2021-07-10 00:42:38 +0600

    recap

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
 // next 33/3
-
-import './App.css';
+import React from 'react'
+import './App.css'
+import MovieCounter from './components/MovieCounter';
+import Nayok from './components/Nayok'
 
 function App() {
-  const nayoks = ['Anwar', 'Samman', 'Hugosab', 'Jackobs', 'Landmand', 'Hervey']
-  const products = [
-    { name: 'Photoshop', price: '$90.99' },
-    { name: 'Illustrator', price: '$60.99' },
-    { name: 'PDF Reader', price: '$6.99' },
-    { name: 'Premiere El', price: '$249.99' }
-  ]
 
+  const nayoks = ['SAFI', 'LARK', 'JACK', 'MONA']
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <p>I am a React Learner!!!</p>
-        <ul>
-          {
-            nayoks.map(nayok => <li>{nayok}</li>)
-          }
-          {
-            products.map(product => <li>{product.name}</li>)
-          }
-        </ul>
-          {
-            products.map(pd => <Product product={pd}></Product>)
-        }
-      </header>
+      <MovieCounter />
+      <Nayok name={nayoks[0]} age="25" />
+      <Nayok name={nayoks[1]} />
+      <Nayok name={nayoks[2]} />
+      <Nayok name={nayoks[3]} />
     </div>
   );
-}
-
-
-function Product(props) {
-  const productStyle = {
-    border: '5px solid purple',
-    borderRadius: '5px',
-    backgroundColor: 'red',
-    height: '200px',
-    width: '200px',
-    float: 'left'
-  }
-  return (
-    <div style={productStyle}>
-      <h3>{props.product.name}</h3>
-      <h5>{props.product.price}</h5>
-      <button>Buy Now</button>
-    </div>
-  )
 }
 
 export default App;
