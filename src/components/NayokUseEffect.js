cd9ac02d644089c8949de7f2f35f04d42f31ebfe
safi <safisafikuls@gmail.com>
2021-07-10 01:56:20 +0600

    clean

--- src/components/NayokUseEffect.js
+++ src/components/NayokUseEffect.js
@@ -10,17 +10,6 @@ function NayokUseEffect() {
         .then(data => setNayoks(data))
     }, [])
     
-
-    // const nayoks = [
-    //     { name: 'SAFI', age: 28 },
-    //     { name: 'LARK', age: 38 },
-    //     { name: 'JACK', age: 40 },
-    //     { name: 'MONA', age: 38 },
-    //     { name: 'MARK', age: 55 },
-    //     { name: 'ARMAN', age: 60 },
-    //     { name: 'LUMAN', age: 84 },
-    //     { name: 'ROKS', age: 100}
-    // ]
     return (
         <div>
             {
